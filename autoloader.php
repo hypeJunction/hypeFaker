bba46443373ccc2507ecb4d84827b559a0d72133
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-05 09:28:16 +0100

    feat(core): drop support for earlier Elgg versions
    BREAKING CHANGE: this remove support for versions of Elgg earlier that 2.0

--- autoloader.php
+++ autoloader.php
@@ -5,7 +5,3 @@ if (file_exists("{$plugin_root}/vendor/autoload.php")) {
 	// check if composer dependencies are distributed with the plugin
 	require_once "{$plugin_root}/vendor/autoload.php";
 }
-
-if (\hypeJunction\Integration::isElggVersionBelow('1.9.0')) {
-	require_once "{$plugin_root}/lib/forward_compat.php";
-}
--- lib/forward_compat.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace hypeJunction\Faker;
-
-function elgg_create_river_item($options = array()) {
-	return add_to_river($options['view'], $options['action_type'], $options['subject_guid'], $options['object_guid'], $options['access_id'], $options['posted'], $options['annotation_id']);
-}
